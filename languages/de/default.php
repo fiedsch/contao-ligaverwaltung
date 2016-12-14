1b378c3f6aec43e97d05cc42a4cae2d9b2716cac
fiedsch <fiedsch@ja-eh.at>
2016-12-14 17:23:17 +0100

    add labels for content elements

---
+++ languages/de/default.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package Ligaverwaltung
+ * @link https://github.com/fiedsch/contao-ligaverwaltung/
+ * @license https://opensource.org/licenses/MIT
+ */
+
+$GLOBALS['TL_LANG']['CTE']['mannschaftsliste'] = array(
+    'Mannschaftsliste',
+    'Mannschaftsliste aus den Daten, die im Backend in der Ligaverwaltung erfasst werden',
+);
+
+$GLOBALS['TL_LANG']['CTE']['spielerliste'] = array(
+    'Spielerliste',
+    'Spielerliste aus den Daten, die im Backend in der Ligaverwaltung erfasst werden',
+);
