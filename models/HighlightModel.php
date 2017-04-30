aa2e77cf9938379a5e7daddbafc104b28a6693de
fiedsch <fiedsch@ja-eh.at>
2017-04-30 10:50:08 +0200

    weiterer Highlight-typ

--- models/HighlightModel.php
+++ models/HighlightModel.php
@@ -11,6 +11,7 @@ class HighlightModel extends \Model
     const TYPE_180 = 1;
     const TYPE_SHORTLEG = 2;
     const TYPE_HIGHFINISH = 3;
+    const TYPE_171 = 4;
 
     /**
      * Table name
@@ -25,8 +26,9 @@ class HighlightModel extends \Model
     public static function getOptionsArray()
     {
         return [
-            self::TYPE_180 => '180',
-            self::TYPE_SHORTLEG => 'Short Leg',
+            self::TYPE_180        => '180',
+            self::TYPE_171        => '171',
+            self::TYPE_SHORTLEG   => 'Short Leg',
             self::TYPE_HIGHFINISH => 'High Finish',
         ];
     }
