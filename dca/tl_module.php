4c36670d089c7d5133e768bfdb8b0c61d8dcdf62
Andreas Fieger <fiedsch@ja-eh.at>
2018-02-24 10:29:28 +0100

    liga wird hier nicht benötigt

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,7 +1,8 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['spielortseitenreader'] = '{title_legend},name,headline,type;{config_legend},liga';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['spielortseitenreader'] = '{title_legend},name,headline,type'; // {config_legend},liga';
 
+/*
 $GLOBALS['TL_DCA']['tl_module']['fields']['liga'] = [
         'label'            => &$GLOBALS['TL_LANG']['tl_module']['liga'],
         'inputType'        => 'checkboxWizard',
@@ -14,3 +15,4 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['liga'] = [
         'options_callback' => ['\Fiedsch\Liga\DCAHelper', 'getLigaForSelect'],
         'sql'              => "blob NULL",
 ];
+*/
--- languages/de/tl_module.php
+++ languages/de/tl_module.php
@@ -1,5 +1,7 @@
 <?php
+/*
 $GLOBALS['TL_LANG']['tl_module']['liga'] = [
-    'Ligen',
-    'Ligen, die in diesem Module berücksichtigt werden sollen'
+    'Liga',
+    'Liga, die in diesem Module berücksichtigt werden sollen'
 ];
+*/
